8d167c4ea196c3672d9474719fa36087d96b60a7
kenhsu <s988662211@gmail.com>
2020-01-31 00:27:37 +0800

    Initialize project.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(HotReloadApp());
+}
+
+class HotReloadApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.tealAccent,
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Center(child: Text('Hot here'),),
+        ),
+        body: SafeArea(
+            child: Container(
+              height: 300.0,
+              width: 300.0,
+              margin: EdgeInsets.all(50.0),
+              color: Colors.white70,
+              child: Center(child: Text('Safe here')),
+            )
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
